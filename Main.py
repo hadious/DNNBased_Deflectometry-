d1f0f35c36a5f1f53ca004c1e2be6eb3e67ad24f
Hadi <mohammad.hadi.sepanj@gmail.com>
2024-02-25 15:58:11 -0500

    test/Valid/Visualization added

--- Main.py
+++ Main.py
@@ -6,6 +6,7 @@ import torch.nn as nn
 import torch.optim as optim 
 from tqdm import tqdm
 from torchvision import transforms
+from torch.utils.data import random_split
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print ("Running on "+ str(device))
@@ -54,7 +55,7 @@ def Main():
     parser.add_argument(
         "--num_epochs",
         type=int,
-        default=10
+        default=3
     )
     parser.add_argument(
         "--height",
@@ -66,6 +67,17 @@ def Main():
         type=int,
         default=256
     )
+    parser.add_argument(
+        "--TrainSplit",
+        type=float,
+        default=0.7
+    )
+    parser.add_argument(
+        "--ValidationSplit",
+        type=float,
+        default=0.15
+    )
+     
     options = parser.parse_args()
 
     datset_path = options.dataset_path
@@ -76,11 +88,24 @@ def Main():
     num_epochs = options.num_epochs
     height = options.height
     width = options.width
-
+    TrainSplit = options.TrainSplit
+    ValidationSplit = options.ValidationSplit
+ 
     ###################################################################################
 
     dataset = Surface_Dataset(datset_path, image_suffix, depthMap_suffix, transform) 
-    data_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True,)
+    data_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
+ 
+
+    train_size = int(TrainSplit * len(dataset))   
+    val_size = int(ValidationSplit * len(dataset))    
+    test_size = len(dataset) - train_size - val_size   
+
+    train_set, val_set, test_set = random_split(dataset, [train_size, val_size, test_size])
+
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True)
+    val_loader = torch.utils.data.DataLoader(val_set, batch_size=batch_size, shuffle=False)
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=False)
 
 
     model = UNet().to(device)
@@ -91,28 +116,80 @@ def Main():
     model.train()
     for epoch in range(num_epochs):
         running_loss = 0.0
-        with tqdm(data_loader, desc=f'Epoch {epoch+1}/{num_epochs}', unit='batch') as t:
+        with tqdm(train_loader, desc=f'Epoch {epoch+1}/{num_epochs}', unit='batch') as t:
             for i, (images, depth_maps) in enumerate(t):
                 images, depth_maps = images.to(device), depth_maps.to(device)
-                
-                # Zero the parameter gradients
                 optimizer.zero_grad()
-
-                # Forward pass
                 outputs = model(images)
-
-                # Compute the loss
                 loss = criterion(outputs, depth_maps)
                 # loss = custom_loss_function(outputs, depth_maps, height, width)
-
-                # Backward pass and optimize
                 loss.backward()
                 optimizer.step()
-                # Print statistics
                 running_loss += loss.item()
-        print(f'Epoch {epoch + 1}, Loss: {running_loss / len(data_loader)}')
-                
+        print(f'Epoch {epoch + 1}, Loss: {running_loss / len(data_loader)}')               
     print('Finished Training')
 
 
+    # Validation phase
+    model.eval()
+    val_loss = 0.0
+    with torch.no_grad():
+        for images_val, depth_maps_val in val_loader:
+            images_val, depth_maps_val = images_val.to(device), depth_maps_val.to(device)
+            outputs_val = model(images_val)
+            loss_val = criterion(outputs_val, depth_maps_val)
+            val_loss += loss_val.item()
+    print(f'Validation - Epoch {epoch + 1}, Loss: {val_loss / len(val_loader)}')
+    
+    # Testing phase
+    model.eval()
+    test_loss = 0.0
+    with torch.no_grad():
+        for images_test, depth_maps_test in test_loader:
+            images_test, depth_maps_test = images_test.to(device), depth_maps_test.to(device)
+            outputs_test = model(images_test)
+            loss_test = criterion(outputs_test, depth_maps_test)
+            test_loss += loss_test.item()
+    print(f'Test Loss: {test_loss / len(test_loader)}')
+
+
+    import matplotlib.pyplot as plt
+
+    # Set model to evaluation mode
+    model.eval()
+
+    # Iterate through the test set and get predictions
+    with torch.no_grad():
+        for images_test, depth_maps_test in test_loader:
+            images_test, depth_maps_test = images_test.to(device), depth_maps_test.to(device)
+            outputs_test = model(images_test)
+
+            # Convert tensors to numpy arrays
+            images_test_np = images_test.cpu().numpy()
+            depth_maps_test_np = depth_maps_test.cpu().numpy()
+            outputs_test_np = outputs_test.cpu().numpy()
+
+            # Plot the images, ground truth, and predictions
+            for i in range(len(images_test_np)):
+                plt.figure(figsize=(10, 5))
+
+                plt.subplot(1, 3, 1)
+                plt.imshow(images_test_np[i].transpose(1, 2, 0))
+                plt.title('Input Image')
+                plt.axis('off')
+
+                plt.subplot(1, 3, 2)
+                plt.imshow(depth_maps_test_np[i].squeeze(), cmap='gray')
+                plt.title('Ground Truth Depth Map')
+                plt.axis('off')
+
+                plt.subplot(1, 3, 3)
+                plt.imshow(outputs_test_np[i].squeeze(), cmap='gray')
+                plt.title('Predicted Depth Map')
+                plt.axis('off')
+
+                plt.show()
+    
+
 Main()
+
